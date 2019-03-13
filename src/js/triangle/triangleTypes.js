41407c9e0a2f5fb98022212e87c14754dc7b7d0f
kolombet <kkolombet@gmail.com>
2019-03-13 22:52:49 +0300

    triangle types

---
+++ src/js/triangle/triangleTypes.js
@@ -0,0 +1,6 @@
+export const INVALID = "INVALID";
+export const EQUILATERAL = "EQUILATERAL";
+export const ISOSCELES = "ISOSCELES";
+export const SCALENE = "SCALENE";
+export const RIGHT = "RIGHT";
+export const PYTHAGOREAN = "PYTHAGOREAN";
